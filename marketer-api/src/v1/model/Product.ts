2f3f4afef80c3bf39fcebb7337a5fcda544b0f7c
Guilherme-Joviniano <00drpixelss@gmail.com>
2023-03-01 11:25:21 -0300

    adding model of product in marketer api to handle this model

--- marketer-api/src/v1/model/Product.ts
+++ marketer-api/src/v1/model/Product.ts
@@ -2,17 +2,49 @@ import db from '../libs/prisma';
 
 class Product {
   async create(data: {
+    ownerId: number,
+    name: string
     price: number,
+    price_type: boolean,
     category: {
       name: string,
       id: number
     },
+    available_quantity: number
   }) {
-    const res = await db.product.create({
-      data: {
+    try {
+      const res = await db.product.create({
+        data: {
+          marketerId: data.ownerId,
+          category_of_productId: data.category.id,
+          price: data.price,
+          price_type: data.price_type,
+          name: data.name,
+          available_quantity: data.available_quantity,
+        },
+      });
 
-      },
-    });
+      return { error: false, data: res };
+    } catch (error) {
+      if (error instanceof Error) {
+        return { error: true, message: 'Failed to save new product in database', code: 401 };
+      }
+    }
+  }
+
+  async disable(id: number) {
+    try {
+      await db.product.update({
+        where: { id },
+        data: {
+          active_for_selling: false,
+        },
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        return { error: true, message: 'Failed to disable this product, id is wrong!', code: 401 };
+      }
+    }
   }
 }
 
