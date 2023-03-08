343520247003b18091ad7426763a704a821d92cc
Guilherme-Joviniano <00drpixelss@gmail.com>
2023-03-08 08:13:06 -0300

    fixing server port

--- commons-api/src/v1/www/server.ts
+++ commons-api/src/v1/www/server.ts
@@ -2,8 +2,8 @@ import server from '../Server';
 
 async function run() {
   await server.listen({
-    port: 3333,
-    
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+    host: '0.0.0.0',
   });
 }
 
