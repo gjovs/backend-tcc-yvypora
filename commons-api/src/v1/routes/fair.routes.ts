e98b5d0db8f83f6c1be4f35a7d76b2312a7b1efe
= <=>
2023-04-16 22:17:21 -0300

    modifing fair.routes

--- commons-api/src/v1/routes/fair.routes.ts
+++ commons-api/src/v1/routes/fair.routes.ts
@@ -1,7 +1,4 @@
 import { FastifyInstance, FastifyRequest } from 'fastify';
-import { orderByDistance } from 'geolib';
-import { GeolibInputCoordinates } from 'geolib/es/types';
-import { Fair } from '../services';
 import { FairController } from '../controllers';
 
 export default async function fairRoutes(server: FastifyInstance) {
